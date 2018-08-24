8a3f8bd0629e5952054ccf0704c411fb2dee0a68
viviannncui <viviannn@umich.edu>
2018-08-24 10:31:06 -0400

    about next method

--- generator.py
+++ generator.py
@@ -12,3 +12,24 @@ def create_generator():
 generator = create_generator()
 for ele in generator:
     print(ele)
+
+
+
+About next method
+Think of it this way:
+An iterator is just a fancy sounding term for an object that has a next() method.
+
+generator = create_generator()
+print generator will print an object
+
+This is the next method
+def square_numbers(list):
+    for ele in list:
+        yield (ele*ele)
+
+numbers = square_numbers([1,2,3,4,5])
+print next(numbers) #1
+print next(numbers) #4
+print next(numbers) #9
+print next(numbers) #16
+print next(numbers) #25
